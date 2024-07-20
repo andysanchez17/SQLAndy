833ff60cbe8bee6a46d9b8bd350d0285f62a584f
andysanchez17 <andreasanchezmaldonado17@gmail.com>
2024-07-19 19:36:17 -0700

    Update clase 1.sql

--- mis querys/clase 1.sql
+++ mis querys/clase 1.sql
@@ -1,5 +1,21 @@
 -- aqui estoy seleccionando todos los nombres de los homnbres de lqa base de datos person 
 --select * from Person WHERE gender = 'male'
 
---
-select * from Person WHERE gender = 'male'
+-- esto nos sirve para tomar todos los que sean menores de 18 
+--select * from Person WHERE gender = 'male' and old < 18
+
+--select * from Person WHERE gender = 'male' and old  > 18
+
+--select * from Person WHERE gender = 'male' and old >18 and old <30
+
+--sirve de una manera mas de buscar entre edades 
+--select * from Person WHERE gender = 'male' and old BETWEEN 18 and 30
+
+--select * from Person WHERE gender = 'female' and old < 18
+
+--select * from Person WHERE gender = 'female' and old BETWEEN 18 and 30
+
+--select * from Person WHERE old BETWEEN 18 and 30
+
+
+
