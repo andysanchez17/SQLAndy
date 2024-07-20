0d922147d508bf4782d40bfd43de19f2a04cd159
andysanchez17 <andreasanchezmaldonado17@gmail.com>
2024-07-19 19:19:54 -0700

    mi primer push

---
+++ mis querys/clase 1.sql
@@ -0,0 +1,5 @@
+-- aqui estoy seleccionando todos los nombres de los homnbres de lqa base de datos person 
+--select * from Person WHERE gender = 'male'
+
+--
+select * from Person WHERE gender = 'male'
